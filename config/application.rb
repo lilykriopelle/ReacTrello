8842812a9d3c4d0c32cdc537309581674c1d6796
Lily Riopelle <lilykriopelle@gmail.com>
2016-04-14 18:08:21 -0400

    Troubleshoot assets.

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module ReacTrello
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_files = true
 
     config.paperclip_defaults = {
       :storage => :s3,
